1797588bf63dfc66f8c1a2beb870c4a7459418d6
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-04-17 06:31:24 -0300

    [Refactor] Added UUID generation function in field insertion method to provide unique identifier and avoid rendering and animation inconsistencies

--- src/components/DataCreation/FormFields/InstallmentsListFormField/index.tsx
+++ src/components/DataCreation/FormFields/InstallmentsListFormField/index.tsx
@@ -1,5 +1,7 @@
 import { SvgIcon, TextField } from "@material-ui/core";
 import { Clear } from "@material-ui/icons";
+import { AnimatePresence, motion } from "framer-motion";
+import { nanoid } from "nanoid";
 import React from "react";
 import NumberFormat from "react-number-format";
 import { FormFieldComponentProps } from "../Root";
@@ -23,15 +25,19 @@ const InstallmentsListFormField = ({
   value,
 }: FormFieldComponentProps) => {
   const handleInsertField = () => {
-    setFieldValue(name, [...value, ""], false);
+    setFieldValue(
+      name,
+      [...value, { installmentMonths: "", installmentValue: "", id: nanoid() }],
+      false
+    );
   };
 
   const handleRemoveAtIndex = (indexArg: number) => {
-    let currentFieldArray: any[] = [...value];
-
-    currentFieldArray.splice(indexArg, 1);
-
-    console.log(currentFieldArray);
+    let currentFieldArray: any[] = (value as any[]).filter(
+      (value, index: number) => {
+        return value.id !== indexArg;
+      }
+    );
 
     setFieldValue(name, [...currentFieldArray], true);
   };
@@ -39,52 +45,74 @@ const InstallmentsListFormField = ({
   return (
     <ListBaseFormFieldLayout insertField={handleInsertField} label={"Label"}>
       {(value as any[]).length > 0 ? (
-        (value as any[]).map((value, index: number) => {
-          return (
-            <ListFormItemLayout
-              key={index}
-              alignCenter
-              removeField={() => handleRemoveAtIndex(index)}
-            >
-              <InstallmentFormFieldWrapper>
-                <NumberFormat
-                  className="installmentNumber"
-                  variant="outlined"
-                  label="Número de parcelas"
-                  isNumericString
-                  value={value[index]?.["installmentMonths"]}
-                  customInput={TextField}
-                  name={`${name}.${index}.installmentMonths`}
-                  onValueChange={({ floatValue, formattedValue, value }) => {
-                    setFieldValue(`${name}.${index}.installmentMonths`, value);
-                  }}
-                />
-                <SvgIcon component={Clear} />
+        <AnimatePresence initial={false}>
+          {(value as any[]).map((valueInner, index: number) => {
+            return (
+              <motion.div
+                key={valueInner["id"]}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5 }}
+                exit={{ opacity: 0, x: -100 }}
+                initial={{ opacity: 0, x: -100 }}
+              >
+                <ListFormItemLayout
+                  key={index}
+                  alignCenter
+                  removeField={() => handleRemoveAtIndex(valueInner["id"])}
+                >
+                  <InstallmentFormFieldWrapper>
+                    <NumberFormat
+                      key={index}
+                      className="installmentNumber"
+                      variant="outlined"
+                      label="Número de parcelas"
+                      isNumericString
+                      value={valueInner?.["installmentMonths"]}
+                      customInput={TextField}
+                      name={`${name}.${index}.installmentMonths`}
+                      onValueChange={({
+                        floatValue,
+                        formattedValue,
+                        value,
+                      }) => {
+                        setFieldValue(
+                          `${name}.${index}.installmentMonths`,
+                          value
+                        );
+                      }}
+                    />
+                    <SvgIcon component={Clear} />
 
-                <NumberFormat
-                  value={value[index]?.["installmentValue"]}
-                  name={`${name}.${index}.installmentValue`}
-                  onValueChange={({ floatValue, formattedValue, value }) => {
-                    setFieldValue(
-                      `${name}.${index}.installmentValue`,
-                      floatValue
-                    );
-                  }}
-                  className="installmentValue"
-                  variant="outlined"
-                  label="Valor das parcelas"
-                  thousandSeparator={"."}
-                  decimalSeparator={","}
-                  allowNegative={false}
-                  fixedDecimalScale={true}
-                  prefix={"R$"}
-                  decimalScale={2}
-                  customInput={TextField}
-                />
-              </InstallmentFormFieldWrapper>
-            </ListFormItemLayout>
-          );
-        })
+                    <NumberFormat
+                      value={valueInner?.["installmentValue"]}
+                      name={`${name}.${index}.installmentValue`}
+                      onValueChange={({
+                        floatValue,
+                        formattedValue,
+                        value,
+                      }) => {
+                        setFieldValue(
+                          `${name}.${index}.installmentValue`,
+                          floatValue
+                        );
+                      }}
+                      className="installmentValue"
+                      variant="outlined"
+                      label="Valor das parcelas"
+                      thousandSeparator={"."}
+                      decimalSeparator={","}
+                      allowNegative={false}
+                      fixedDecimalScale={true}
+                      prefix={"R$"}
+                      decimalScale={2}
+                      customInput={TextField}
+                    />
+                  </InstallmentFormFieldWrapper>
+                </ListFormItemLayout>
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
       ) : (
         <div style={{ textAlign: "start" }}>
           Parece que ainda não há nenhum item na lista.
